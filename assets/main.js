f100fe88e8d07602f04cb2862467442aa9a17a90
lucky-patty <luck.wtj@gmail.com>
2019-09-01 04:09:13 -0700

    add comment to main.js

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,21 @@
+// Put everytime you gonna write jQuery
 $(document).ready(function(){
 
+  // Hide element
   $("#show").hide();
 
+  // Add Click funtion to element
   $("#test").click(function(){
 
+    // Check whether element is hide
     if($("#show").is(":hidden")){
+      // If hide then show element
       $("#show").show();
     }else{
+      // If show then hide element
       $("#show").hide();
-    }  
+    }
+
   });
 
 });
