e4e938acf37151fff43ec3600ca3598b60d67dcd
lucky-patty <luck.wtj@gmail.com>
2019-09-01 04:06:42 -0700

    add logic hide/show in main.js

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
   $("#show").hide();
 
   $("#test").click(function(){
-    $("#show").show();
+
+    if($("#show").is(":hidden")){
+      $("#show").show();
+    }else{
+      $("#show").hide();
+    }  
   });
 
 });
