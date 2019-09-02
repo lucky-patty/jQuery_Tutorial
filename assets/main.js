093be816fccc5c029be1c5be0b5cd285220ab0ef
lucky-patty <luck.wtj@gmail.com>
2019-09-02 03:06:14 -0700

    update tutorial for jquery in html

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,33 @@
 // Put everytime you gonna write jQuery
 $(document).ready(function(){
 
+  // First Page
+
+  // HTML with jQuery
+
+  // AJAX
+  $("#randomId").click(function(){
+
+  });
+
   // Hide element
   $("#show").hide();
 
   // Add Click funtion to element
   $("#test").click(function(){
 
-    // Check whether element is hide
+    // // Check whether element is hide
     if($("#show").is(":hidden")){
       // If hide then show element
       $("#show").show();
-    }else{
-      // If show then hide element
-      $("#show").hide();
     }
+    // else{
+    //   // If show then hide element
+    //   $("#show").hide();
+    // }
 
   });
+
+  
 
 });
---
+++ assets/page2.js
@@ -0,0 +1,8 @@
+// jQuery Start!
+$(document).ready(function(){
+
+  $("#button").click(function(){
+      alert('The Button has been click!');
+  });
+
+});
